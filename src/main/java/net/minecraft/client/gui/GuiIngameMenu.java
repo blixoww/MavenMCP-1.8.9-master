278a24722dd19010441d52924c450fa1da692b55
blixoww <valentin.houpert@free.fr>
2026-03-28 00:47:37 +0100

    New pause menu

--- src/main/java/net/minecraft/client/gui/GuiIngameMenu.java
+++ src/main/java/net/minecraft/client/gui/GuiIngameMenu.java
@@ -9,34 +9,33 @@ import net.minecraft.realms.RealmsBridge;
 
 public class GuiIngameMenu extends GuiScreen
 {
-    private int field_146445_a;
-    private int field_146444_f;
-
     /**
      * Adds the buttons (and other controls) to the screen in question. Called when the GUI is displayed and when the
      * window resizes, the buttonList is cleared beforehand.
      */
     public void initGui()
     {
-        this.field_146445_a = 0;
         this.buttonList.clear();
         int i = -16;
-        int j = 98;
-        this.buttonList.add(new GuiButton(1, this.width / 2 - 100, this.height / 4 + 120 + i, I18n.format("menu.returnToMenu", new Object[0])));
-
-        if (!this.mc.isIntegratedServerRunning())
-        {
-            ((GuiButton)this.buttonList.get(0)).displayString = I18n.format("menu.disconnect", new Object[0]);
-        }
-
-        this.buttonList.add(new GuiButton(4, this.width / 2 - 100, this.height / 4 + 24 + i, I18n.format("menu.returnToGame", new Object[0])));
+        
+        // Bouton "Reprendre" en haut, bien large
+        this.buttonList.add(new GuiButton(4, this.width / 2 - 100, this.height / 4 + 24 + i, 200, 20, "§a" + I18n.format("menu.returnToGame", new Object[0])));
+        
+        // Ligne Succès et Stats
+        this.buttonList.add(new GuiButton(5, this.width / 2 - 100, this.height / 4 + 48 + i, 98, 20, I18n.format("gui.achievements", new Object[0])));
+        this.buttonList.add(new GuiButton(6, this.width / 2 + 2, this.height / 4 + 48 + i, 98, 20, I18n.format("gui.stats", new Object[0])));
+        
+        // Bouton "Editer l'UI" (plus important sur un serveur PvP)
+        this.buttonList.add(new GuiButton(8, this.width / 2 - 100, this.height / 4 + 72 + i, 200, 20, "§eEditer l'UI"));
+        
+        // Ligne Options et LAN (ou autre)
         this.buttonList.add(new GuiButton(0, this.width / 2 - 100, this.height / 4 + 96 + i, 98, 20, I18n.format("menu.options", new Object[0])));
         GuiButton guibutton;
         this.buttonList.add(guibutton = new GuiButton(7, this.width / 2 + 2, this.height / 4 + 96 + i, 98, 20, I18n.format("menu.shareToLan", new Object[0])));
-        this.buttonList.add(new GuiButton(5, this.width / 2 - 100, this.height / 4 + 48 + i, 98, 20, I18n.format("gui.achievements", new Object[0])));
-        this.buttonList.add(new GuiButton(6, this.width / 2 + 2, this.height / 4 + 48 + i, 98, 20, I18n.format("gui.stats", new Object[0])));
-        this.buttonList.add(new GuiButton(8, this.width / 2 - 100, this.height / 4 + 72 + i, 200, 20, "Editer l'UI")); // Nouveau bouton pour ouvrir l'éditeur d'UI
         guibutton.enabled = this.mc.isSingleplayer() && !this.mc.getIntegratedServer().getPublic();
+
+        // Bouton Quitter/Déconnexion en bas, en rouge
+        this.buttonList.add(new GuiButton(1, this.width / 2 - 100, this.height / 4 + 120 + i, 200, 20, "§c" + (this.mc.isIntegratedServerRunning() ? I18n.format("menu.returnToMenu", new Object[0]) : I18n.format("menu.disconnect", new Object[0]))));
     }
 
     /**
@@ -105,7 +104,6 @@ public class GuiIngameMenu extends GuiScreen
     public void updateScreen()
     {
         super.updateScreen();
-        ++this.field_146444_f;
     }
 
     /**
@@ -114,7 +112,11 @@ public class GuiIngameMenu extends GuiScreen
     public void drawScreen(int mouseX, int mouseY, float partialTicks)
     {
         this.drawDefaultBackground();
-        this.drawCenteredString(this.fontRendererObj, I18n.format("menu.game", new Object[0]), this.width / 2, 40, 16777215);
+        
+        // Titre personnalisé
+        this.drawCenteredString(this.fontRendererObj, "§6§lRed§f§lConflict", this.width / 2, 40, 16777215);
+        this.drawCenteredString(this.fontRendererObj, "§7Menu de jeu", this.width / 2, 52, 16777215);
+
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 }
